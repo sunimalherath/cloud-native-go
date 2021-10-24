79193f801ddc497f28aa10b89dd6a65bf38e0515
Sunimal Herath <sunimaldv@gmail.com>
2021-10-25 00:07:27 +1100

    implementing simple http server with go

---
+++ microservice.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
+	http.ListenAndServe(port(), nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w,"welcome to the cloud native go...")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+	w.Header().Add("content-type", "text/plain")
+	fmt.Fprintf(w, message)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = ":8080"
+	}
+	return ":" + port
+}
